d8a7dd7ef7968fdf3966ff4423df00640e90d07d
astr0 <astro@astr0s-MacBook-Pro.local>
2023-08-25 18:35:29 +0000

    Metric form section calculation completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const radioSection = document.querySelector('#radio-section');
 const metric = document.querySelector('#Metric-form');
 const imperial = document.querySelector('#Imperial-form');
+const category = document.querySelector('#category')
+
+
 
 //PLAN
 //*****
@@ -31,9 +34,51 @@ function radioSwitch(e) {
 }
 
 //Metric Form Calculation
+//Formulae [weight (kg) / height (cm) / height (cm)] x 10,000.
+//Target the Height input field in kg.
+//Target the Weight field in cm.
+//Target the result.
+//Target the category of BMI and Description
+
+function metricField(e) {
+    //Metric Selector Weight and input fields
+    const heightInput = metric.querySelector('#MetricHeight');
+    const weightInput = metric.querySelector('#MetricWeight');
+    const bmi = document.querySelector('#BMI');
+    const h1 = category.querySelector('#one');
+    const h2 = category.querySelector('#two');
+
+    //Covert the input value to integer
+    const Heightvalue = heightInput.value;
+    const weightValue = weightInput.value;
+    HeightString = parseInt(Heightvalue);
+    weightString = parseInt(weightValue);
+    //use the formula to calculate the BMI
+    const BMI = ((weightString / HeightString / HeightString) * 10000).toFixed(2);
+    bmi.innerHTML = BMI;
+
+    //Category
+    if (BMI <18.5) {
+        h1.innerText = 'Underweight'
+        
+    }
+    if (BMI >=18.5) {
+        h1.innerText = 'Normal weight';
+    }
+    if (BMI >=25) {
+        h1.innerText = 'Overweight';
+    }
+    if (BMI >=30) {
+        h1.innerText = 'Obeese';
+    }
+    
+}
+
+
 
 //Imperial Form Calculation
 
 
 //add event Listeners
 radioSection.addEventListener('change', radioSwitch);
+metric.addEventListener('input', metricField);
